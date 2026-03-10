8b5c299d699cc2d1d52bb58cc0009a2e341ff481
Iryna <krasii.iryna@gmail.com>
2026-03-10 22:47:17 +0200

    resolve rebase conflict and update config

---
+++ src/core.d.ts
@@ -0,0 +1,3 @@
+export declare function processEvent(event: string | null): string;
+export declare function logEvent(event: string): void;
+//# sourceMappingURL=core.d.ts.map
--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,14 @@
-export function processEvent(event: string): string {
-  return `[EVENT]: ${event}`;
+export function processEvent(event: string | null): string {
+  if (!event) {
+    return "Empty event";
+  }
+
+  const message = `[EVENT]: ${event}`;
+  console.log(`Log event: ${event}`);
+
+  return message;
+}
+
+export function logEvent(event: string) {
+  console.log(`Log event: ${event}`);
 }
