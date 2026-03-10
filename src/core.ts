f54420c25ec4ec42d5dd320ce789dfbc17854f87
Iryna <krasii.iryna@gmail.com>
2026-03-10 22:25:02 +0200

    change event format

--- src/core.ts
+++ src/core.ts
@@ -1,7 +1,3 @@
-export function processEvent(event: string | null): string {
-  if (!event) {
-    return "Empty event";
-  }
-
-  return `Processing event: ${event}`;
+export function processEvent(event: string): string {
+  return `[EVENT]: ${event}`;
 }
